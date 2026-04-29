234e635eac335baf5164c703a23370e7aee900db
sat7312 <sat7312@gmail.com>
2026-04-29 09:50:51 +0900

    5 반복문

---
+++ src/main/kotlin/ch5/Main.kt
@@ -0,0 +1,11 @@
+package com.back.ch5
+
+fun main(args: Array<String>) {
+    for(i in 0 until  5) {
+        println("Count: ${i}")
+    }
+
+    for(i in 0 ..  4) {
+        println("Count: ${i}")
+    }
+}
