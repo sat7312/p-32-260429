74dc27a3985256f4d849b9e2f3f72a95636af006
sat7312 <sat7312@gmail.com>
2026-04-29 10:20:26 +0900

    7 클래스와 객체

---
+++ src/main/kotlin/ch7/Person.kt
@@ -0,0 +1,13 @@
+package com.back.ch7
+
+class Person(val name: String)
+{
+    fun greet() {
+        println("Hello, my name is $name")
+    }
+}
+
+fun main() {
+    val person = Person("Alice")
+    person.greet()
+}
