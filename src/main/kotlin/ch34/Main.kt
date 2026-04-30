82306c2fa641a8dfb0a4c20fc3f14de3aee2d0c8
sat7312 <sat7312@gmail.com>
2026-04-30 09:26:12 +0900

    33 open 키워드

---
+++ src/main/kotlin/ch34/Main.kt
@@ -0,0 +1,18 @@
+package com.back.ch34
+
+open class Animal { // 이제 상속 가능
+    open fun makeSound() {
+        println("Some generic animal sound")
+    }
+}
+
+class Dog : Animal() {
+    override fun makeSound() { // 메서드 오버라이딩 가능
+        println("Bark! Bark!")
+    }
+}
+
+fun main() {
+    val dog = Dog()
+    dog.makeSound() // 출력: Bark! Bark!
+}
